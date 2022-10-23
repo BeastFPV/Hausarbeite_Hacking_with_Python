d2b2e91a7ca091ca188604fd7fcaa21fcc999239
BeastFPV <tobiesma@hs-albsig.de>
2022-10-23 13:55:25 +0200

    test_pypi feature/vuln

---
+++ setup copy.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='pythonSftpServer',
+    version='1.0.0',
+    description='Python SFTP Server connection client and server',
+    url='https://github.com/BeastFPV/Hausarbeite_Hacking_with_Python',
+    author='BeastFPV',
+    author_email='beastfpv12@gmail.com',
+    license='BSD 2-clause',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
